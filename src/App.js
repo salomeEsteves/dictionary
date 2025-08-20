582e41088f866ff998413e445ab948a3350ea332
salomeEsteves <r.salome.esteves@gmail.com>
2025-08-20 11:25:36 +0100

    created form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary.js"
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Hi!</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <p>Coded by Salomé Esteves</p>
+      </footer>
+      </div>
     </div>
   );
 }
