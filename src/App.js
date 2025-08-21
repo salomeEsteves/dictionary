84f7ae8182998ef95b94afb7534cc9afb4d7b9f1
salomeEsteves <r.salome.esteves@gmail.com>
2025-08-21 14:31:44 +0100

    perfected the byline

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Dictionary defaultKeyword="life"/>
       </main>
       <footer>
-        <p className="byline">Coded by Salomé Esteves</p>
+        <p className="byline">🧑🏻‍💻 Coded by <a href="https://www.linkedin.com/in/salome-esteves/" target="_blank" rel="noreferrer" className="link-dark">Salomé Esteves</a>, open-sourced in <a href="https://github.com/salomeEsteves/dictionary/tree/main/src" target="_blank" rel="noreferrer" className="link-dark">Github</a> and hosted on <a href="https://monumental-tapioca-f1c1ef.netlify.app/" target="_blank" rel="noreferrer" className="link-dark">Netlify</a></p>
       </footer>
       </div>
     </div>
